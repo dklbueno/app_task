4bf6f264fe51dcc785e41152bddc034d90fe6117
daniel.silva <daniel.silva@idealinvest.com.br>
2017-06-05 15:19:00 -0300

    Correção de bug de configuração

--- src/Duo/ModelBundle/DependencyInjection/Configuration.php
+++ src/Duo/ModelBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sac\ModelBundle\DependencyInjection;
+namespace Duo\ModelBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
